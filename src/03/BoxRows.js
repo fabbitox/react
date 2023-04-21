171d0126af01dd375cc34d8ac3fef34a0cc338ca
fabbitox <hyunjin_11@naver.com>
2023-04-21 13:10:56 +0900

    box

---
+++ src/03/BoxRows.js
@@ -0,0 +1,39 @@
+// const BoxRows = (props) => {
+const BoxRows = ({mvs}) => {
+    // const mvs = props.mvs;
+    // console.log("boxrows", mvs);
+
+    let trTags = [];
+    for (let mv of mvs) {
+        // console.log(mv.rank, mv.movieNm, mv.salesAmt, mv.rankInten);
+        let inten = parseInt(mv.rankInten);
+        let rankEmoji = getRankEmoji(inten);
+        trTags.push(
+            <tr className="mvtr" key={mv.movieCd}>
+                <td>{mv.rank}</td>
+                <td>{mv.movieNm}</td>
+                <td><span  className="price">&#8361; {parseInt(mv.salesAmt).toLocaleString()}</span></td>
+                <td>{rankEmoji} {Math.abs(inten)}</td>
+            </tr>
+        );
+    }
+    // console.log(trTags);
+
+    return (
+        <>
+            {trTags}
+        </>
+    );
+}
+
+const getRankEmoji = (rankInten) => {
+    if (rankInten == 0) {
+        return '⏺';
+    } else if (rankInten > 0) {
+        return '🔼';
+    } else {
+        return '🔽';
+    }
+}
+
+export default BoxRows;
--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-//import Hello from './01/Hello';
-import './01/Hello.css';
-import MyClock from './02/MyClock';
+import Box from './03/Box';
+import './03/Box.css';
 
 function App() {
   return (
-    <MyClock />
+    <Box />
   );
 }
 
