095331ae32a1f62cc45a3c3b3f461782a9dfb9cf
fabbitox <hyunjin_11@naver.com>
2023-05-04 13:10:40 +0900

    0504

--- src/07/MyRefForm.js
+++ src/07/MyRefForm.js
@@ -1,7 +1,26 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import './MyRef.module.css';
 
 const MyRefForm = () => {
+    const txtref = useRef();
+    useEffect(() => {
+        txtref.current.focus();
+    }, []);
+
+    const itemArr = useRef([]);
+    const [itemTag, setItemTage] = useState();
+
+    const addItem = (e) => {
+        e.preventDefault();
+        itemArr.current = [...itemArr.current, txtref.current.value];
+        console.log('add item =', itemArr.current);
+    };
+
+    const resetItem = (e) => {
+        e.preventDefault();
+        console.log('reset item');
+    };
+
     return (
         <main className="container">
             <article>
@@ -9,10 +28,13 @@ const MyRefForm = () => {
                     <form>
                         <div className="grid">
                             <div>
-                                <label htmlFor="txt1">과일/채소 입력</label>
-                                <input type="text" id="txt1" name="txt1" required />
+                                <label htmlFor="txt">과일/채소 입력</label>
+                                <input ref={txtref} type="text" id="txt" name="txt" required />
                             </div>
-                            <div><button>등록</button><button>취소</button></div>
+                            <div>
+                                <button onClick={(e) => addItem(e)}>등록</button>
+                                <button onClick={(e) => resetItem(e)}>취소</button>
+                            </div>
                         </div>
                     </form>
                 </header>
