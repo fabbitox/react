538536129e6e82c36ef0df57245530a9cc1692be
fabbitox <hyunjin_11@naver.com>
2023-05-23 12:54:47 +0900

    calcuate time

--- src/10/VilageFcst.js
+++ src/10/VilageFcst.js
@@ -10,15 +10,53 @@ const VilageFcst = () => {
     const x = useParams().x;
     const y = useParams().y;
     const cg = useRef();
-    const options = code.filter((item) => item.예보구분 === "단기예보").map((item) => <option value={item.항목명} key={item.항목명}>{item.항목명}</option>);
+    const options = code.filter((item) => item.예보구분 === "단기예보").map((item) => <option value={item.항목값 + " " + item.단위} key={item.항목명}>{item.항목명}</option>);
     const [fcst, setFcst] = useState();
+    const [category, setCg] = useState();
+    const [unit, setUnit] = useState();
 
     const displayDt = (dt) => {
         return dt.substr(0, 4) + '/' + dt.substr(4, 2) + '/' + dt.substr(6, 2);
     }
 
+    const isToday = (dt) => {
+        let date = new Date().getDate();
+        date = (date < 10 ? "0" : "") + date;
+        if (date === dt.substr(6, 2))
+            return true;
+        return false;
+    }
+
+    const changeCt = () => {
+        const value = cg.current.value.split(" ");
+        setCg(value[0]);
+        if (value[1].startsWith("범주"))
+            setUnit('');
+        else
+            setUnit(value[1]);
+    }
+
     useEffect(() => {
-        let time = '0500';
+        const date = new Date();
+        let hour = date.getHours();
+        let minute = date.getMinutes();
+        let offset;
+        if (isToday(dt)) {
+            offset = 1000 * 60 * minute * -1;
+            offset -= ((hour % 3) + 1) * 60 * 60 * 1000;
+            if (minute < 10) {
+                offset -= 1000 * 60 * 60;
+            }
+        } else {
+            offset = 1000 * 60 * minute * -1;
+            hour += 1;
+            offset -= ((hour % 3) - 3) * 60 * 60 * 1000;
+        }
+        const dateNew = new Date(date.getTime() + offset);
+        hour = dateNew.getHours();
+        minute = dateNew.getMinutes();
+        const time = (hour < 10 ? "0" : "") + hour + (minute < 10 ? "0" : "") + minute;
+        console.log(time);
         let url = "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=lRsZfmJLjf7hvQqhA568pBWCTmyVfaTgEjP%2Bk9pXXrXNfh0DVuCIQ4NP4yt25SkXeLkQCUlA1K7tUpEnZxHtmQ%3D%3D&pageNo=1&numOfRows=1000&dataType=JSON&";
         url += `base_date=${dt}&base_time=${time}&nx=${x}&ny=${y}`;
         fetch(url).then((resp) => resp.json()).then((data) => setFcst(data.response.body.items.item))
@@ -28,13 +66,13 @@ const VilageFcst = () => {
     return (
         <article>
             <header><div className="grid">
-                <div>{area} 단기예보 ({displayDt(dt)})</div>
-                <select id="category" name="category" ref={cg}>
+                <div className={style.fcstitle}>{area} 단기예보 ({displayDt(dt)})</div>
+                <select id="category" name="category" ref={cg} onChange={changeCt}>
                     <option value='' className={style.opt}>선택</option>
                     {options}
                 </select>
             </div></header>
-            <FcstTable fcst={fcst} />
+            {fcst && <FcstTable fcst={fcst} cg={category} unit={unit} />}
         </article>
     )
 }
